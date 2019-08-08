a5948f7a92727549a8f3b92ecaf23c55bb49cf53
micahhahn <micah.s.hahn@gmail.com>
2019-08-08 17:33:36 -0500

    Factoring

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,31 +1,82 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TemplateHaskell #-} 
+{-# LANGUAGE TypeFamilies #-}
+
 module Lib
     ( someFunc
     ) where
 
+import Data.Functor.Foldable
+import Data.Functor.Foldable.TH
+import Data.List (intercalate)
 import Data.Sparse.SpMatrix
 import Data.Sparse.SpVector
 import Numeric.LinearAlgebra.Sparse
 
-a :: SpMatrix Double
-a = fromListDenseSM 2 [ 1, 3
+a' :: SpMatrix Double
+a' = fromListDenseSM 2 [ 1, 3
                       , 1, 1]
 
-b :: SpVector Double
-b = mkSpVR 2 [1, 3]
+b' :: SpVector Double
+b' = mkSpVR 2 [1, 3]
 
 data Exp a = Add (Exp a) (Exp a)
            | Mul (Exp a) (Exp a)
            | Var Char
            | Lit a
-           deriving (Show)
+           deriving (Show, Eq)
+
+instance (Num a) => Num (Exp a) where
+    negate = (Lit (-1) `Mul`)
+    (+) = Add
+    (*) = Mul
+    fromInteger = Lit . fromInteger
+    abs = id
+    signum = id
+
+makeBaseFunctor ''Exp
+
+simp :: (Num a) => Exp a -> Exp a
+simp e = cata f e
+    where f ((l `Add` r) `MulF` y) = (l * y) + (r * y)
+          f (x `MulF` (l `Add` r)) = (x * l) + (x * r)
+          f x = embed x
 
 simplify :: (Num a) => Exp a -> Exp a
-simplify (Mul (Con a) (Con b)) = Con (a * b) 
+{- Always distribute multiplications -}
+simplify ((Add l r) `Mul` y) = (l * y) + (r * y)
+simplify (x `Mul` (Add l r)) = (x * l) + (x * r)
 simplify x = x
 
-x = (Var 'a' `Mul` Var 'a') `Mul` Con 1.0
+simplifyR :: (Num a, Eq a) => Exp a -> [Exp a]
+simplifyR e = let e' = simp e
+               in if e == e' then [e] else e : simplifyR e'
+
+p :: (Show a) => [Exp a] -> String
+p es = intercalate "\n" (pretty <$> es)
+
+pretty :: (Show a) => Exp a -> String
+pretty (l `Add` r) = pretty l ++ " + " ++ pretty r
+pretty (x `Mul` y) = let x' = case x of
+                                  (_ `Add` _) -> "(" ++ pretty x ++ ")"
+                                  _ -> pretty x
+                         y' = case y of 
+                                  (_ `Add` _) -> "(" ++ pretty y ++ ")"
+                                  _ -> pretty y
+                      in x' ++ y'
+pretty (Var c) = [c]
+pretty (Lit a) = show a
+
+a = Var 'a'
+b = Var 'b'
+c = Var 'c'
+d = Var 'd'
+e = Var 'e'
+f = Var 'f'
+
+test = ((a + b) * (c + d)) * (e + f)
 
 someFunc :: IO ()
 someFunc = do
-    putStrLn (show a)
-    putStrLn (show b)
+    putStrLn (show test)
